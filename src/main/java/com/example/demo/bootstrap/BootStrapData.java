a6e765190dbd821ba11bf34ae56b0a497c790220
Iodas <michi.michi@outlook.com>
2023-04-24 09:41:58 +0200

    added publisher entity with repository

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.example.demo.bootstrap;
 
 import com.example.demo.domain.Author;
 import com.example.demo.domain.Book;
+import com.example.demo.domain.Publisher;
 import com.example.demo.repositories.AuthorRepository;
 import com.example.demo.repositories.BookRepository;
+import com.example.demo.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -23,11 +27,13 @@ public class BootStrapData implements CommandLineRunner {
 
         Author eric = new Author("Eric,", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
+        Publisher orell = new Publisher("Orell", "hauptbahnhofstrasse 1", "Zürich", "Zürich", 8000L);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(orell);
 
         Author rod = new Author("Rod,", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "54528754545");
@@ -39,6 +45,7 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
-        
+        System.out.println("Number of Publishers: " + publisherRepository.count());
+
     }
 }
