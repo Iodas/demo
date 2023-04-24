ea80b370195b46eb285bf16d7f00f4988a2cb53e
Iodas <michi.michi@outlook.com>
2023-04-24 10:47:17 +0200

    added mapping of book to publisher

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -27,25 +27,35 @@ public class BootStrapData implements CommandLineRunner {
 
         Author eric = new Author("Eric,", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
-        Publisher orell = new Publisher("Orell", "hauptbahnhofstrasse 1", "Zürich", "Zürich", 8000L);
+        Publisher publisher = new Publisher("Orell", "hauptbahnhofstrasse 1", "Zürich", "Zürich", 8000L);
+        publisherRepository.save(publisher);
+
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
-        publisherRepository.save(orell);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod,", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "54528754545");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
         System.out.println("Number of Publishers: " + publisherRepository.count());
+        System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
 
     }
 }
